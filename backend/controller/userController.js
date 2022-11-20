0b2bd5160687b43eb3884ea7191fc562c1dff447
Dawa <sherpadawa01@outlook.com>
2022-11-20 15:37:13 +0545

    user register done

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,33 @@
-import user from '../model/userModel.js';
-export const registerUser = async (req, res) => {
-  if (!req.body.email) {
-    res.status(404).json({ message: 'please add an email' });
-    throw new Error('Please add an email');
+import User from '../model/userModel.js';
+import asyncHandler from 'express-async-handler';
+export const registerUser = asyncHandler(async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //validation
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error('Please fill in all required fields');
+    }
+    if (password.length < 6) {
+      res.status(400);
+      throw new Error('Password must be at least 6 characters');
+    }
+    //check user email if already exists;
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      res.status(400);
+      throw new Error('Email is already in use');
+    }
+    //Create new user;
+    const user = await User.create({ name, email, password });
+    if (user) {
+      await user.save();
+      res.status(201).json(user);
+    } else {
+      res.status(500);
+      throw new Error('User was able to create');
+    }
+  } catch (error) {
+    res.json({ error: error });
   }
-};
+});
