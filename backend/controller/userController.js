26eaaeffd03d5ff6492ce241c4e7ae6af9dae896
Dawa <sherpadawa01@outlook.com>
2022-11-20 15:21:04 +0545

    error handler work done

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,7 @@
 import user from '../model/userModel.js';
 export const registerUser = async (req, res) => {
   if (!req.body.email) {
-    res.status(404);
+    res.status(404).json({ message: 'please add an email' });
     throw new Error('Please add an email');
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,25 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 //importing routes;
 import userRoutes from './routes/userRoutes.js';
+//error handler import;
+import { errorHandler } from './middleware/errorMiddleware.js';
+
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(cors());
-mongo();
 app.use(morgan('dev'));
+app.use(cors());
+//database connection
+mongo();
 
 //Routes;
 app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 5000;
+
+//ERROR MIDDLEWARE;
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`listening on server ${PORT}`);
